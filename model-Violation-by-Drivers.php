718b659bee37932097d14a28c53869e61446804e
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-10-23 13:28:33 -0500

    Create model-Violation-by-Drivers.php

---
+++ model-Violation-by-Drivers.php
@@ -0,0 +1,15 @@
+<?php
+function selectViolation() {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("SELECT Violation_id, Violation_number, Violation_reason FROM 'Violation'")
+    $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
