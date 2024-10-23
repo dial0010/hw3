966bd819d28f7c7f8529c07874ab57090246e95a
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-10-23 13:30:56 -0500

    Update model-Violation-by-Drivers.php

--- model-Violation-by-Drivers.php
+++ model-Violation-by-Drivers.php
@@ -2,7 +2,7 @@
 function selectViolation() {
   try {
     $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT Violation_id, Violation_number, Violation_reason FROM `Drivers`")
+    $stmt = $conn->prepare("SELECT Violation_id, Violation_number, Violation_reason FROM `Violation`")
     $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
