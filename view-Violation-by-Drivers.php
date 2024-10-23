b57482ce046dbb8e4d9433098cf2aa71f3c62f0a
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-10-23 13:47:28 -0500

    Create view-Violation-by-Drivers.php

---
+++ view-Violation-by-Drivers.php
@@ -0,0 +1,31 @@
+<h1>Violation by Drivers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+      <th>Number</th>
+      <th>Reason</th>
+         <th>Month</th>
+         <th>Day</th>
+         <th>Day/ Time</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($Violation = $Violation->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $Violation['Violation_id']; ?></td>
+    <td><?php echo $Violation['Violation_number']; ?></td>
+    <td><?php echo $Violation['Violation_reason']; ?></td>
+  <td><?php echo $Violation['Month']; ?></td>
+  <td><?php echo $Violation['Day']; ?></td>
+  <td><?php echo $Violation['Day_time']; ?></td>
+</tr>
+      <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
