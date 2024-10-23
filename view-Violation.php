18790425e7ed48eaec485f48e38f3a20a292e69f
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-10-23 13:15:00 -0500

    Update view-Violation.php

--- view-Violation.php
+++ view-Violation.php
@@ -1,1 +1,25 @@
-
+<h1>Violation</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+      <th>Number</th>
+      <th>Reason</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($Violation = $Violation->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $Violation['Violation_id']; ?></td>
+    <td><?php echo $Violation['Violation_number']; ?></td>
+    <td><?php echo $Violation['Violation_reason']; ?></td>
+</tr>
+      <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
