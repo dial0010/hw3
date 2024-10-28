8706c8c74db8888ba5c2344319a6eb230ca83aa6
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-10-28 13:34:32 -0500

    Update model-Violation-by-Drivers.php

--- model-Violation-by-Drivers.php
+++ model-Violation-by-Drivers.php
@@ -1,9 +1,10 @@
 <?php
-function selectViolation() {
+function selectViolationbyDrivers($iid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT V.Violation_id, Violation_number, Violation_reason, Month, Day, Day_time FROM `Violation` V join City C on V.Violation_id = C.City_id where C.Drivers_id=?");
-     $stmt->execute();
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
