351eb9daefa581ab5890dff6e0e294c1f7b5cb5a
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-04 11:25:24 -0600

    Create model-City-by-Driver.php

---
+++ model-City-by-Driver.php
@@ -0,0 +1,16 @@
+<?php
+function selectViolationbyDrivers($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT V.Violation_id, Violation_number, Violation_reason, Month, Day, Day_time FROM `Violation` V join City C on V.Violation_id = C.City_id where C.Drivers_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>    
