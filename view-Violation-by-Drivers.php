e7457ae3562bd82652dcc4761e3eecf4186634fa
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-04 11:16:28 -0600

    Update view-Violation-by-Drivers.php

--- view-Violation-by-Drivers.php
+++ view-Violation-by-Drivers.php
@@ -13,7 +13,7 @@
     </thead>
     <tbody>
 <?php
-while ($Violations = $Violation->fetch_assoc()) {
+while ($Violation = $Violation->fetch_assoc()) {
 ?>
 <tr>
   <td><?php echo $Violation['Violation_id']; ?></td>
