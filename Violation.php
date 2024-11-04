12555e2b26881ee553f55275040710c6b29fdd5d
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-04 10:32:02 -0600

    Update Violation.php

--- Violation.php
+++ Violation.php
@@ -4,7 +4,7 @@ require_once("model-Violation.php");
 
 $pageTitle = "Violations";
 include "view-header.php";
-$Violations = selectViolations();
+$Violations = selectViolation();
 include "view-Violations.php";
 include "view-footer.php";
 ?>
