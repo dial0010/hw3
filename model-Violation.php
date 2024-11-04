7bdf38ca72c5b846f25e126be9ca8f6f18509166
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-04 10:48:33 -0600

    Update model-Violation.php

--- model-Violation.php
+++ model-Violation.php
@@ -1,5 +1,5 @@
 <?php
-function selectViolationByDrivers($iid) {
+function selectViolation() {
   try {
     $conn = get_db_connection();
   $stmt = $conn->prepare("SELECT Violation_id, Violation_number, Violation_reason FROM `Violation`");
