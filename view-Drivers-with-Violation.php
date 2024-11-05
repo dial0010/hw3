de2f17caed1e9089c93dd8272122bae7e006d774
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-05 17:48:07 -0600

    Update view-Drivers-with-Violation.php

--- view-Drivers-with-Violation.php
+++ view-Drivers-with-Violation.php
@@ -6,16 +6,18 @@ while ($Driver = $Drivers->fetch_assoc()) {
   <div class="card">
     <div class="card-body">
       <h5 class="card-title"><?php echo $Driver['Drivers_name']; ?></h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      <p class="card-text">
+        <?php
+  $Violation = selectViolationbyDrivers($Driver['Drivers_id']);
+  while($Violation = $Violation->fetchassoc()) {
+    ?>
+  <?php
+  }
+  ?>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Car Brand: <?php echo $Driver['Car_brand']; ?></small></p>
     </div>
   </div>
-<tr>
-  <td><?php echo $Driver['Drivers_id']; ?></td>
-    <td><?php echo $Driver['Drivers_name']; ?></td>
-    <td><?php echo $Driver['Car_brand']; ?></td>
-  <td><a href ="Violation-by-Drivers.php?id=<?php echo $Driver['Drivers_id']; ?>">Violation</a></td>
-</tr>
       <?php
 }
 ?>
