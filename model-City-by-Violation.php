0797be0df3723568714eead76e05ef9e1ab83f3b
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-06 13:40:08 -0600

    Update model-City-by-Violation.php

--- model-City-by-Violation.php
+++ model-City-by-Violation.php
@@ -2,7 +2,7 @@
 function selectCitybyViolation($cid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT V.Violation_id, Violation_number, Violation_reason, Month, Day, Day_time FROM `Violation` V join City C on V.Violation_id = C.City_id where V.Violation_id=?");
+        $stmt = $conn->prepare("SELECT V.Violation_id, Violation_number, Violation_reason, Month, Day, Day_time FROM `Violation` V join City C on V.Violation_id = C.City_id where C.Violation_id=?");
         $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
