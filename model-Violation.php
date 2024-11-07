681e911ffbb11dfd66b2c6421c2968d457c2a5a2
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-07 14:15:20 -0600

    Update model-Violation.php

--- model-Violation.php
+++ model-Violation.php
@@ -12,4 +12,43 @@ function selectViolation() {
         throw $e;
     }
 }
+function insertViolation($vNumber, $vReason) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("INSERT INTO `Violation` (`Violation_number`, `Violation_reason`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $vNumber, $vReason);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateViolation($vNumber, $vReason, $Vid) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("update `Violation` set `Violation_number` = ?, `Violation_reason` = ? where Violation_id = ?");
+    $stmt->bind_param("ssi", $vNumber, $vReason, $cid);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteViolation($cid) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("delete from `Violation` where Violation_id = ?");
+    $stmt->bind_param("i", $cid);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
