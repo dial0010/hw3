49bbcfe847ffc24aaf0fa166229a7a7c4c4a321b
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-11 13:49:56 -0600

    Update Violation.php

--- Violation.php
+++ Violation.php
@@ -4,6 +4,12 @@ require_once("model-Violation.php");
 
 $pageTitle = "Violations";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+  }
+}
+
 $Violations = selectViolation();
 include "view-Violation.php";
 include "view-footer.php";
