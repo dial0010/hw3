6e7307324281074cea6bdb363857c78764b92437
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-13 13:59:12 -0600

    Update Violation.php

--- Violation.php
+++ Violation.php
@@ -7,21 +7,21 @@ include "view-header.php";
 
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
-    case "Add";
+    case "Add":
     if (insertViolation($_POST['vNumber'], $_POST['vReason'])) {
     echo '<div class="alert alert-success" role="alert">Course added.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
     break;
-     case "Delete";
+     case "Delete":
     if (deleteViolation($_POST['cid'])) {
     echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
     break;
-     case "Edit";
+     case "Edit":
     if (updateViolation($_POST['vNumber'], $_POST['vReason'], $_POST['cid'])) {
     echo '<div class="alert alert-success" role="alert">Course edited.</div>';
     } else {
