9444b645adc52846717c9ac64263201cf06df178
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-18 13:32:23 -0600

    Update view-Officer.php

--- view-Officer.php
+++ view-Officer.php
@@ -1,1 +1,25 @@
-
+<h1>Officers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+      <th>Name</th>
+      <th>Rank</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($Officer = $Drivers->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $Officer['Officer_id']; ?></td>
+    <td><?php echo $Officer['Officer_name']; ?></td>
+    <td><?php echo $Officer['Officer_rank']; ?></td>
+</tr>
+      <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
