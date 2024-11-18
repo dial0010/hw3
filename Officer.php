c06562f41fe2b44e1cffaf6fbef4c1c888fe1eb3
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-18 13:23:08 -0600

    Update Officer.php

--- Officer.php
+++ Officer.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-Officer.php");
 
+$pageTitle = "Officers";
+include "view-header.php";
+$Officers = selectOfficer();
+Include "view-Officer.php";
+include "view-footer.php";
+?>
