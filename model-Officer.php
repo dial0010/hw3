922b8ff192cc76ae674d88144afd7bd29a5c99dc
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-18 13:25:11 -0600

    Update model-Officer.php

--- model-Officer.php
+++ model-Officer.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectOfficer() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Officer_id, Officer_name, Officer_rank FROM `Officer`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
