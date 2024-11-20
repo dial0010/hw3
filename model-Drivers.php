f2f6a49030afeae1b34848fcb5899bc0c27e1fa3
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-20 13:22:58 -0600

    Update model-Drivers.php

--- model-Drivers.php
+++ model-Drivers.php
@@ -12,4 +12,43 @@ function selectDrivers() {
         throw $e;
     }
 }
+function insertDrivers($dName, $dBrand) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("INSERT INTO `Drivers` (`Drivers_name`, `Car_brand`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $dName, $dBrand);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateDrivers($dName, $dBrand, $did) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("update `Drivers` set `Drivers_name` = ?, `Car_brand` = ? where Drivers_id = ?");
+    $stmt->bind_param("ssi", $dName, $dBrand, $did);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteDrivers($did) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("delete from `Drivers` where Drivers_id = ?");
+    $stmt->bind_param("i", $did);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
