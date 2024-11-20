874fbb6036e51e2f6bad0ab002fd7cc866a53f3f
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-20 13:43:48 -0600

    Update Officer.php

--- Officer.php
+++ Officer.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-    if (insertOfficer($_POST['vNumber'], $_POST['vReason'])) {
+    if (insertOfficer($_POST['oName'], $_POST['oRank'])) {
     echo '<div class="alert alert-success" role="alert">Officer added.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
     break;
      case "Delete":
-    if (deleteOfficer($_POST['cid'])) {
+    if (deleteOfficer($_POST['oid'])) {
     echo '<div class="alert alert-success" role="alert">Officer deleted.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
     }
     break;
      case "Edit":
-    if (updateOfficer($_POST['vNumber'], $_POST['vReason'], $_POST['cid'])) {
+    if (updateOfficer($_POST['oName'], $_POST['oRank'], $_POST['oid'])) {
     echo '<div class="alert alert-success" role="alert">Officer edited.</div>';
     } else {
       echo '<div class="alert alert-danger" role="alert">Error.</div>';
