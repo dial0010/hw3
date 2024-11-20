c7f01c7ca58988e00401c4abfff65976079a4960
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-20 13:58:17 -0600

    Update view-Officer.php

--- view-Officer.php
+++ view-Officer.php
@@ -42,3 +42,20 @@ while ($Officer = $Officers->fetch_assoc()) {
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 
+  </svg>
+  </button>
+</form>
+  </td>
+  <td>
+    <form method="post" action="City-by-Violation.php">
+  <input type="hidden" name="cid" value="<?php echo $Violation['Violation_id']; ?>">
+  <button type="submit" class="btn btn-primary">City</button>
+</form>
+  </td>
+</tr>
+      <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
