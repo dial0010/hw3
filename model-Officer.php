050006e809002033bac9efd5c6ab2564c82e4622
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-20 13:46:56 -0600

    Update model-Officer.php

--- model-Officer.php
+++ model-Officer.php
@@ -12,4 +12,45 @@ function selectOfficer() {
         throw $e;
     }
 }
+function insertOfficer($oName, $oRank) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("INSERT INTO `Officer` (`Office_name`, `Officer_rank`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $oName, $oRank);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateOfficer($oName, $oRank, $oid) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("update `Officer` set `Office_name` = ?, `Officer_rank` = ? where Officer_id = ?");
+    $stmt->bind_param("ssi", $oName, $oRank, $oid);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteOfficer($oid) {
+  try {
+    $conn = get_db_connection();
+  $stmt = $conn->prepare("delete from `Officer` where Officer_id = ?");
+    $stmt->bind_param("i", $oid);
+    $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
