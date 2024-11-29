e2ae54807c20bade4ca4e18a15067b2287b5c167
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-11-29 11:22:20 -0600

    Update City-by-Violation.php

--- City-by-Violation.php
+++ City-by-Violation.php
@@ -4,7 +4,7 @@ require_once("model-City-by-Violation.php");
 
 $pageTitle = "City by Violation";
 include "view-header.php";
-$Cities = selectCitybyViolation($_POST['cid']);
+$Cities = selectCitybyViolation($_POST['did']);
 include "view-City-by-Violation.php";
 include "view-footer.php";
 ?>
