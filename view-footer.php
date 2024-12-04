769f20370fe560781321d2902b3d8ded07f40bf6
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-04 12:32:58 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,9 @@
-</div> <!-- End container -->
+<div class="container mt-5">
+    <footer class="footer py-3 mt-5 bg-dark text-white text-center">
+        <p>&copy; 2024 Parking Oklahoma. All rights reserved.</p>
+    </footer>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
