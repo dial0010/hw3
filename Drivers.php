4bddfb10b6c834da49ff7281cddb0d2286f0c758
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-04 12:38:30 -0600

    Update Drivers.php

--- Drivers.php
+++ Drivers.php
@@ -1,37 +1,39 @@
 <?php
-require_once("util-db.php");
+require_once("util-db.php");  
 require_once("model-Drivers.php");
 
 $pageTitle = "Drivers";
 include "view-header.php";
 
+// Handling form submissions
 if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-    if (insertDrivers($_POST['dName'], $_POST['dBrand'])) {
-    echo '<div class="alert alert-success" role="alert">Driver added.</div>';
-    } else {
-      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertDrivers($_POST['dName'], $_POST['dBrand'])) {
+                echo '<div class="alert alert-success" role="alert">Driver added successfully.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error adding driver.</div>';
+            }
+            break;
+        case "Delete":
+            if (deleteDrivers($_POST['did'])) {
+                echo '<div class="alert alert-success" role="alert">Driver deleted successfully.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error deleting driver.</div>';
+            }
+            break;
+        case "Edit":
+            if (updateDrivers($_POST['dName'], $_POST['dBrand'], $_POST['did'])) {
+                echo '<div class="alert alert-success" role="alert">Driver updated successfully.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error updating driver.</div>';
+            }
+            break;
     }
-    break;
-     case "Delete":
-    if (deleteDrivers($_POST['did'])) {
-    echo '<div class="alert alert-success" role="alert">Driver deleted.</div>';
-    } else {
-      echo '<div class="alert alert-danger" role="alert">Error.</div>';
-    }
-    break;
-     case "Edit":
-    if (updateDrivers($_POST['dName'], $_POST['dBrand'], $_POST['did'])) {
-    echo '<div class="alert alert-success" role="alert">Driver edited.</div>';
-    } else {
-      echo '<div class="alert alert-danger" role="alert">Error.</div>';
-    }
-    break;
-  }
 }
 
+// Fetching all drivers
 $Drivers = selectDrivers();
-Include "view-Drivers.php";
+include "view-Drivers.php";
 include "view-footer.php";
 ?>
