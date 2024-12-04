6a167daff2a18dd9abe0f98c8c9e03ae24780a6d
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-04 13:14:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,10 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
-<h1>Parking Oklahoma</h1>
-
-<!-- Search bar section -->
-<div class="container mt-4">
-  <form action="search-results.php" method="get">
-    <div class="row">
-      <div class="col-12 col-md-8">
-        <input type="text" class="form-control" placeholder="Search for parking violations..." name="query" id="query">
-      </div>
-      <div class="col-12 col-md-4">
-        <button type="submit" class="btn btn-primary w-100">Search</button>
-      </div>
-    </div>
-  </form>
+<!-- Add content for the home page -->
+<div class="text-center text-white">
+  <h1>Welcome to Parking Oklahoma</h1>
+  <p>Your go-to source for managing parking information</p>
 </div>
 
 <?php
