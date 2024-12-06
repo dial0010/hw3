e81a41f45977f4c71601d82c95f1957b31aa512a
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-05 21:15:29 -0600

    Update view-TCourt.php

--- view-TCourt.php
+++ view-TCourt.php
@@ -50,12 +50,6 @@ while ($TCourt = $TCourts->fetch_assoc()) {
   </button>
 </form>
   </td>
-     <td>
-    <form method="post" action="City-by-Violation.php">
-  <input type="hidden" name="did" value="<?php echo $Violations['Drivers_id']; ?>">
-  <button type="submit" class="btn btn-primary">City</button>
-</form>
-  </td>
 </tr>
       <?php
 }
