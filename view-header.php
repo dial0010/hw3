69d94c0160043ffa6a63d75aaebfad082cd9a1bb
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-10 23:50:45 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,30 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Add third-party libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/echarts@5.3.3/dist/echarts.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3.8.0/notyf.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/parsleyjs@2.9.2/dist/parsley.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div class = "container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Drivers.php">Drivers</a>
-        </li>  
-        <li class="nav-item">
-          <a class="nav-link" href="Violation.php">Violation</a>
-        </li>  
-        <li class="nav-item">
-          <a class="nav-link" href="Drivers-with-Violation.php">Drivers with Violation</a>
-        </li>  
-      </ul>
-    </div>
-  </div>
-</nav>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/" id="home-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Drivers.php" id="drivers-link">Drivers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Violation.php" id="violation-link">Violations</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Drivers-with-Violation.php" id="drivers-with-violation-link">Drivers with Violation</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
