2855489e4f4c51212c63b6e3f97a31834e726bff
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-11 00:02:48 -0600

    Update view-Violation.php

--- view-Violation.php
+++ view-Violation.php
@@ -1,32 +1,32 @@
-<h1>Violations</h1>
+<h1 class="animate__animated animate__fadeIn">Violations</h1>
 <div class="table-responsive">
-  <table class="table">
+  <table class="table table-striped table-hover">
     <thead>
       <tr>
         <th>ID</th>
-      <th>Number</th>
-      <th>Reason</th>
+        <th>Number</th>
+        <th>Reason</th>
         <th></th>
       </tr>
     </thead>
     <tbody>
-<?php
-while ($Violation = $Violations->fetch_assoc()) {
-?>
-<tr>
-    <td><?php echo $Violation['Violation_id']; ?></td>
-    <td><?php echo $Violation['Violation_number']; ?></td>
-    <td><?php echo $Violation['Violation_reason']; ?></td>
-  <td>
-    <form method="post" action="City-by-Violation.php">
-  <input type="hidden" name="cid" value="<?php echo $Violation['Violation_id']; ?>">
-  <button type="submit" class="btn btn-primary">City</button>
-</form>
-  </td>
-</tr>
       <?php
-}
-?>
+        while ($Violation = $Violations->fetch_assoc()) {
+      ?>
+      <tr data-aos="fade-up">
+        <td><?php echo $Violation['Violation_id']; ?></td>
+        <td><?php echo $Violation['Violation_number']; ?></td>
+        <td><?php echo $Violation['Violation_reason']; ?></td>
+        <td>
+          <form method="post" action="City-by-Violation.php">
+            <input type="hidden" name="cid" value="<?php echo $Violation['Violation_id']; ?>">
+            <button type="submit" class="btn btn-primary">City</button>
+          </form>
+        </td>
+      </tr>
+      <?php
+        }
+      ?>
     </tbody>
   </table>
 </div>
