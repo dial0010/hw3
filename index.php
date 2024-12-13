d19de14c5ea967eaf35c2f1316bb40c3dcc25e08
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-13 00:29:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,33 @@ include "view-header.php";
   <p>Your go-to source for managing parking information</p>
 </div>
 
+<!-- Parking spots section -->
+<div class="container mt-5">
+  <div class="row text-center">
+    <div class="col-md-4">
+      <div class="parking-spot">
+        <img src="images/spot1.jpg" alt="Spot 1" class="img-fluid">
+        <h3>Spot 1</h3>
+        <p>Location: Downtown Area</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="parking-spot">
+        <img src="images/spot2.jpg" alt="Spot 2" class="img-fluid">
+        <h3>Spot 2</h3>
+        <p>Location: Near City Hall</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="parking-spot">
+        <img src="images/spot3.jpg" alt="Spot 3" class="img-fluid">
+        <h3>Spot 3</h3>
+        <p>Location: Close to University</p>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php
 include "view-footer.php";
 ?>
