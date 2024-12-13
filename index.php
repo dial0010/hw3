422b493ac528124b41ee5c9eaed6b8098e9e9213
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-12-13 00:40:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,23 +14,29 @@ include "view-header.php";
   <div class="row text-center">
     <div class="col-md-4">
       <div class="parking-spot">
-        <img src="https://jmthompson.com/wp-content/uploads/2018/10/parkingdeck_conrad-104-scaled.jpg" alt="Spot 1" class="img-fluid">
-        <h3>Spot 1</h3>
-        <p>Location: Downtown Area</p>
+        <a href="https://www.ou.edu/parking" target="_blank">
+          <img src="https://jmthompson.com/wp-content/uploads/2018/10/parkingdeck_conrad-104-scaled.jpg" alt="OU Parking" class="img-fluid">
+        </a>
+        <h3><a href="https://www.ou.edu/parking" target="_blank">University of Oklahoma Parking</a></h3>
+        <p>Location: University of Oklahoma Campus</p>
       </div>
     </div>
     <div class="col-md-4">
       <div class="parking-spot">
-        <img src="https://www.embarkok.com/assets/images/Parking/Sheridan_Walker.jpg" alt="Spot 2" class="img-fluid">
-        <h3>Spot 2</h3>
-        <p>Location: Near City Hall</p>
+        <a href="https://www.embarkok.com/parking" target="_blank">
+          <img src="https://www.embarkok.com/assets/images/Parking/Sheridan_Walker.jpg" alt="Embark Parking" class="img-fluid">
+        </a>
+        <h3><a href="https://www.embarkok.com/parking" target="_blank">Embark Parking</a></h3>
+        <p>Location: Downtown Oklahoma City</p>
       </div>
     </div>
     <div class="col-md-4">
       <div class="parking-spot">
-        <img src="https://bloximages.newyork1.vip.townnews.com/oudaily.com/content/tncms/assets/v3/editorial/f/0a/f0a2778e-99f5-11e4-893e-d7abb268b478/54b31cd1ee9ad.image.jpg" alt="Spot 3" class="img-fluid">
-        <h3>Spot 3</h3>
-        <p>Location: Close to University</p>
+        <a href="https://downtownokc.com/parking/" target="_blank">
+          <img src="https://bloximages.newyork1.vip.townnews.com/oudaily.com/content/tncms/assets/v3/editorial/f/0a/f0a2778e-99f5-11e4-893e-d7abb268b478/54b31cd1ee9ad.image.jpg" alt="Downtown OKC Parking" class="img-fluid">
+        </a>
+        <h3><a href="https://downtownokc.com/parking/" target="_blank">Downtown OKC Parking</a></h3>
+        <p>Location: Downtown Oklahoma City</p>
       </div>
     </div>
   </div>
